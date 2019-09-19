b277d2b7d2bfcea380c65226487cb80fb8418514
Link <zhumuzhi@icloud.com>
2019-09-19 09:51:20 +0800

    1.创建项目

---
+++ GeekSampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GeekSampleApp
+//
+//  Created by 朱慕之 on 2019/9/19.
+//  Copyright © 2019 朱慕之. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GeekSampleApp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  GeekSampleApp
+//
+//  Created by 朱慕之 on 2019/9/19.
+//  Copyright © 2019 朱慕之. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
