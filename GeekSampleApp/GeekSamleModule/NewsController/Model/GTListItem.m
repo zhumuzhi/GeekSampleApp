4de958b7f5edb185d5f1fae5fee35e93fd17c89e
Zhumz <zhumuzhi@icloud.com>
2020-02-09 09:26:58 +0800

    1.调整

--- GeekSampleApp/GeekSamleModule/NewsController/Model/GTListItem.m
+++ GeekSampleApp/GeekSamleModule/NewsController/Model/GTListItem.m
@@ -10,6 +10,38 @@
 
 @implementation GTListItem
 
+#pragma mark - NSSecureCoding
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (self) {
+        self.category = [aDecoder decodeObjectForKey:@"category"];
+        self.picUrl = [aDecoder decodeObjectForKey:@"picUrl"];
+        self.uniqueKey = [aDecoder decodeObjectForKey:@"uniqueKey"];
+        self.title = [aDecoder decodeObjectForKey:@"title"];
+        self.date = [aDecoder decodeObjectForKey:@"date"];
+        self.authorName = [aDecoder decodeObjectForKey:@"authorName"];
+        self.articleUrl = [aDecoder decodeObjectForKey:@"articleUrl"];
+    }
+    return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:self.category forKey:@"category"];
+    [aCoder encodeObject:self.picUrl forKey:@"picUrl"];
+    [aCoder encodeObject:self.uniqueKey forKey:@"uniqueKey"];
+    [aCoder encodeObject:self.title forKey:@"title"];
+    [aCoder encodeObject:self.date forKey:@"date"];
+    [aCoder encodeObject:self.authorName forKey:@"authorName"];
+    [aCoder encodeObject:self.articleUrl forKey:@"articleUrl"];
+}
+
++ (BOOL)supportsSecureCoding {
+    return YES;
+}
+
+#pragma mark - Public method
+
 - (void)configWithDictionary:(NSDictionary *)dictionary {
 #warning 注意类型是否匹配
     self.category = [dictionary objectForKey:@"category"];
--- GeekSampleApp/Support/Utils/GTLoder/GTListLoader.m
+++ GeekSampleApp/Support/Utils/GTLoder/GTListLoader.m
@@ -20,6 +20,10 @@
     NSURL *listURL = [NSURL URLWithString:urlString];
     NSURLRequest *listRequest = [[NSURLRequest alloc] initWithURL:listURL];
     NSURLSession *session = [NSURLSession sharedSession];
+    
+    __weak typeof(self)weakSelf = self;
+    
+    
     NSURLSessionTask *dataTask = [session dataTaskWithRequest:listRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         //        NSLog(@"请求的数据data：%@，请求的结果response：%@，请求中的错误：%@", data , response, error);
         
@@ -43,9 +47,15 @@
             }
         });
         
+        [weakSelf _archiveListDataArray:listItemArray.copy];
+        
     }];
     
     [dataTask resume];
+    
+    
+    
+    
     
 // AFN测试代码
 //    [[AFHTTPSessionManager manager] GET:urlString parameters:nil progress:^(NSProgress * _Nonnull downloadProgress)
@@ -60,9 +70,88 @@
 //    {
 //        NSLog(@"error:%@", error);
 //    }];
-    
 
 }
+
+- (void)_archiveListDataArray:(NSArray<GTListItem *> *)array {
+    
+    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *cachePath = [pathArray firstObject];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    // 拼接新建文件夹的地址
+    NSString *dataPath = [cachePath stringByAppendingPathComponent:@"GTData"];
+    
+    NSError *creatError;
+    // 创建文件夹地址
+    [fileManager createDirectoryAtPath:dataPath withIntermediateDirectories:YES attributes:nil error:&creatError];
+    
+    // 创建list文件Path
+    NSString *listDataPath = [dataPath stringByAppendingPathComponent:@"list"];
+    
+    
+//    // 查询文件
+//    BOOL fileExist = [fileManager fileExistsAtPath:listDataPath];
+//    
+//    // 删除文件
+//    if (fileExist) {
+//        [fileManager removeItemAtPath:listDataPath error:nil];
+//    }
+    
+    // 序列化后存储文件
+    NSData *listData = [NSKeyedArchiver archivedDataWithRootObject:array requiringSecureCoding:YES error:nil];
+    
+    [fileManager createFileAtPath:listDataPath contents:listData attributes:nil];
+    
+    // 取出文件后反序列化
+    NSData *readListData = [fileManager contentsAtPath:listDataPath];
+    
+    id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class], [GTListItem class], nil] fromData:readListData error:nil];
+    
+    NSLog(@"%@", unarchiveObj);
+    
+}
+
+- (void)_getSandBoxPath {
+    
+    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+    NSString *cachePath = [pathArray firstObject];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+    // 拼接新建文件夹的地址
+    NSString *dataPath = [cachePath stringByAppendingPathComponent:@"GTData"];
+    
+    NSError *creatError;
+    // 创建文件夹地址
+    [fileManager createDirectoryAtPath:dataPath withIntermediateDirectories:YES attributes:nil error:&creatError];
+    
+    // 创建list文件
+    NSString *listDataPath = [dataPath stringByAppendingPathComponent:@"list"];
+    // 以UTF8的编码方式创建由字符串abc转化的NSData
+    NSData *listData = [@"abc" dataUsingEncoding:NSUTF8StringEncoding];
+    [fileManager createFileAtPath:listDataPath contents:listData attributes:nil];
+    
+    // 查询文件
+//    BOOL fileExist = [fileManager fileExistsAtPath:listDataPath];
+    
+    // 删除文件
+//    if (fileExist) {
+//        [fileManager removeItemAtPath:listDataPath error:nil];
+//    }
+    
+    // 使用fileHandler操作文件
+    NSFileHandle *fileHandler = [NSFileHandle fileHandleForUpdatingAtPath:listDataPath];
+    
+    [fileHandler seekToEndOfFile];
+    [fileHandler writeData:[@"DEF" dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    [fileHandler synchronizeFile];
+    [fileHandler closeFile];
+    
+}
+
 
 @end
 
