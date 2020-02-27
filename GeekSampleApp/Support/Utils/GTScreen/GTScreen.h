4975dd5f0fbb485fbab373b5ec2012ee4334e42e
Zhumz <zhumuzhi@icloud.com>
2020-02-27 16:43:38 +0800

    分辨率计算

--- GeekSampleApp/Support/Utils/GTScreen/GTScreen.h
+++ GeekSampleApp/Support/Utils/GTScreen/GTScreen.h
@@ -10,23 +10,22 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// 判断是否为横屏
 #define IS_LANDSCAPE (UIInterfaceOrientationIsLandscape([[UIApplication sharedApplication] statusBarOrientation]))
 
+// 横竖屏 宽高对调
 #define SCREEN_WIDTH (IS_LANDSCAPE ? [[UIScreen mainScreen ] bounds].size.height : [[UIScreen mainScreen ] bounds].size.width)
-
 #define SCREEN_HEIGHT (IS_LANDSCAPE ? [[UIScreen mainScreen ] bounds].size.width : [[UIScreen mainScreen ] bounds].size.height)
-
-#define STATUSBARHEIGHT (IS_IPHONE_X_XR_MAX ? 44 : 20)
 
 #define UI(x) UIAdapter(x)
 
 #define UIRect(x,y,width,height) UIRectAdapter(x,y,width,height)
 
+#define iPhone6Width 375 // 适配基准使用iPhone6定位
+
+//方式2 - 屏幕宽度按比例适配（使用）
 static inline NSInteger UIAdapter (float x){
-    //1 - 分机型 特定的比例
-    
-    //2 - 屏幕宽度按比例适配
-    CGFloat scale = 414 / SCREEN_WIDTH;
+    CGFloat scale = iPhone6Width / SCREEN_WIDTH;
     return (NSInteger)x /scale;
 }
 
@@ -34,6 +33,11 @@ static inline NSInteger UIAdapter (float x){
 static inline CGRect UIRectAdapter(x,y,width,height){
     return CGRectMake(UIAdapter(x), UIAdapter(y), UIAdapter(width), UIAdapter(height));
 }
+
+//方式1 - 分机型 特定的比例（未实现）
+
+#define STATUSBARHEIGHT (IS_IPHONE_X_XR_MAX ? 44 : 20)
+
 
 
 @interface GTScreen : NSObject
